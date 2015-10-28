2492e9ed92ffca507ea1124029fd3f513135bcf7
Rumbler <rumbler@rellcom.com.br>
2015-10-28 05:46:40 -0300

    Reestrutura drag drop

--- progs_sync/configuracao.cpp
+++ progs_sync/configuracao.cpp
@@ -1,16 +1,35 @@
 #include "configuracao.h"
+#include <QDebug>
 #include <QMimeData>
 #include <QDragEnterEvent>
 #include <QFile>
+#include <QFileInfo>
+#include <QFileDialog>
+#include <QStandardPaths>
 
 Configuracao::Configuracao(QWidget *parent) :
     QMainWindow(parent)
 {
     setupUi(this);
+
+    // habilita drag and drop
     setAcceptDrops(true);
+
+    // posiciona o TabWidget no centro
     setCentralWidget(tabWidget);
+    tabWidget->setCurrentIndex(1);
+
+    // esconde as barras de progressso
     progressFile->hide();
     progressTotal->hide();
+
+    // conexões gerais
+
+}
+
+QStringList Configuracao::listaArquivos() const
+{
+    return m_listaArquivos;
 }
 
 void Configuracao::dragEnterEvent(QDragEnterEvent *event)
@@ -36,7 +55,80 @@ void Configuracao::dropEvent(QDropEvent *event)
     urls = event->mimeData()->urls();
     for(i = urls.begin(); i != urls.end(); ++i){
         fileName = i->toLocalFile();
-      listFile->addItem(fileName);
-    }
+        //QFile file(fileName);
+        QListWidgetItem *item = new QListWidgetItem(listFile);
 
+        item->setText(fileName);
+        //listFile->ins
+        qDebug() << fileName;
+    }
+    listFile->update();
+}
+
+void Configuracao::setListaArquivos(QStringList listaArquivos)
+{
+    if (m_listaArquivos == listaArquivos)
+        return;
+
+    m_listaArquivos = listaArquivos;
+    emit listaArquivosChanged(listaArquivos);
+}
+
+void Configuracao::on_bntAdicionar_clicked()
+{
+    QStringList fileNames;
+    QStringList::iterator i;
+    QString homeLocation = QStandardPaths::writableLocation( QStandardPaths::HomeLocation );
+    QFileDialog dialog(this);
+    dialog.setDirectory(homeLocation);
+    dialog.setFileMode(QFileDialog::ExistingFiles);
+
+    if (dialog.exec())
+        fileNames = dialog.selectedFiles();
+    for(i = fileNames.begin(); i != fileNames.end(); ++i){
+        QListWidgetItem *item = new QListWidgetItem(listFile);
+
+        item->setText(*i);
+    }
+}
+
+void Configuracao::on_bntAdcionarDiretorio_clicked()
+{
+    QStringList fileNames;
+    QStringList::iterator i;
+    QString homeLocation = QStandardPaths::writableLocation( QStandardPaths::HomeLocation );
+    QFileDialog dialog(this);
+    dialog.setDirectory(homeLocation);
+    dialog.setFileMode(QFileDialog::DirectoryOnly);
+
+    if (dialog.exec())
+        fileNames = dialog.selectedFiles();
+    for(i = fileNames.begin(); i != fileNames.end(); ++i){
+        QListWidgetItem *item = new QListWidgetItem(listFile);
+
+        item->setText(*i);
+    }
+}
+
+void Configuracao::on_bntRemover_clicked()
+{
+    QListWidgetItem *item = listFile->takeItem(listFile->currentRow());
+    listFile->removeItemWidget(item);
+}
+
+void Configuracao::on_btnEnviar_clicked()
+{
+
+}
+
+void Configuracao::on_btnCancelar_clicked()
+{
+
+}
+
+
+
+void Configuracao::on_bntLimpar_clicked()
+{
+    listFile->clear();
 }
--- progs_sync/configuracao.h
+++ progs_sync/configuracao.h
@@ -2,30 +2,48 @@
 #define CONFIGURACAO_H
 
 #include "ui_configuracao.h"
+#include <QStringListModel>
 
 class Configuracao : public QMainWindow, private Ui::Configuracao
 {
     Q_OBJECT
-    //Q_PROPERTY(QStringList listaArquivos READ listaArquivos WRITE setListaArquivos NOTIFY listaArquivosChanged)
+    Q_PROPERTY(QStringList listaArquivos READ listaArquivos WRITE setListaArquivos NOTIFY listaArquivosChanged)
     //Q_PROPERTY(int quantidadeArquivos READ quantidadeArquivos WRITE setQuantidadeArquivos NOTIFY quantidadeArquivosChanged)
 
 
 public:
     explicit Configuracao(QWidget *parent = 0);
 
+    QStringList listaArquivos() const;
+
+
 protected:
     void dragEnterEvent(QDragEnterEvent * event);
     void dragLeaveEvent(QDragLeaveEvent * event);
     void dragMoveEvent(QDragMoveEvent * event);
     void dropEvent(QDropEvent * event);
-public slots:
+
+public slots:    
+    void setListaArquivos(QStringList listaArquivos);
 
 
 signals:
+    void listaArquivosChanged(QStringList listaArquivos);
 
+private slots:
+    void on_bntAdicionar_clicked();
+    void on_bntAdcionarDiretorio_clicked();
+    void on_bntRemover_clicked();
+    void on_btnEnviar_clicked();
+    void on_btnCancelar_clicked();
+
+
+
+    void on_bntLimpar_clicked();
 
 private:
-
+    QStringListModel *m_listModel;    
+    QStringList m_listaArquivos;
 };
 
 #endif // CONFIGURACAO_H
